a16d3f9e7e75255581152ef773ad89b73e86c74a
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-19 17:15:39 +0000

    Tue, Dec 19, 2023, 6:15 PM +01:00

--- population.c
+++ population.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
 
+    int currentSize = get_int("Start size: ");
+    int endSize = get_int("End size: ");
+
+    int years = 0;
+
+    do while (currentSize < endSize)
+    {
+        currentSize = calcYearGrowth(currentSize)
+    }
+
+}
+
+int calcYearGrowth(int size)
+{
+    int amountToAdd = size / 3;
+    int amountToSubtract = size / 4;
+
+    return previous + amountToAdd - amountToSubtract;
+}
