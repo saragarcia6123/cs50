d2b5d72e8daf5fcb1d48c7c51cde44a20b56f57d
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-19 18:29:08 +0000

    Tue, Dec 19, 2023, 7:29 PM +01:00

---
+++ scrabble.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int calcTotal(string word);
+
+int[26] charValues = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
+
+int main(void)
+{
+
+}
+
+int calcTotal(char[] letters)
+{
+    int total = 0;
+
+    for (char c : letters)
+    {
+        int index = c.get
+    }
+}
