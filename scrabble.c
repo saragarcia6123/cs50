497d6862eaf6d13082b50abe75a6dd0404aadc21
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-19 18:44:07 +0000

    Tue, Dec 19, 2023, 7:44 PM +01:00

--- scrabble.c
+++ scrabble.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int compute_score(string word);
+char[] convert_input(string input);
 
 int char_values[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
@@ -11,8 +12,11 @@ int main(void)
     string p1 = get_string("Player 1: ");
     string p2 = get_string("Player 2: ");
 
-    int p1_score = compute_score(toupper(p1));
-    int p2_score = compute_score(toupper(p2));
+    p1_char_array = convert_input(p1);
+    p2_char_array = convert_input(p2);
+
+    int p1_score = compute_score(p1_char_array);
+    int p2_score = compute_score(p2_char_array);
 
     printf("Player 1 score: %i", p1_score);
     printf("Player 2 score: %i", p2_score);
@@ -26,6 +30,11 @@ int main(void)
     }
 
     return 0;
+}
+
+char[] convert_input(string input)
+{
+    
 }
 
 int compute_score(char[] letters)
