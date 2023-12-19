c03c95e6ddb7b800bd9e0f8e12225689fc9c02dc
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-19 19:08:24 +0000

    Tue, Dec 19, 2023, 8:08 PM +01:00

--- scrabble.c
+++ scrabble.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int compute_score(string word);
-char[] convert_input(string input);
+char* convert_input(string input);
 bool is_letter(char c);
 
 int char_values[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
@@ -13,8 +13,8 @@ int main(void)
     string p1 = get_string("Player 1: ");
     string p2 = get_string("Player 2: ");
 
-    p1_char_array = convert_input(p1);
-    p2_char_array = convert_input(p2);
+    char p1_char_array[] = convert_input(p1);
+    char p2_char_array[] = convert_input(p2);
 
     int p1_score = compute_score(p1_char_array);
     int p2_score = compute_score(p2_char_array);
@@ -33,7 +33,7 @@ int main(void)
     return 0;
 }
 
-char convert_input(string input)
+char* convert_input(string input)
 {
     int output_length = 0;
 
@@ -45,7 +45,7 @@ char convert_input(string input)
         }
     }
 
-    char output_array[output_length];
+    static char output_array[output_length];
     int char_index = 0;
 
     //create the valid char array
@@ -58,6 +58,8 @@ char convert_input(string input)
             char_index++;
         }
     }
+
+    return output_array;
 }
 
 bool is_letter(char c)
