28e647bb47cc3390c9f52f7358acabb29bcb08d7
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-19 18:33:10 +0000

    Tue, Dec 19, 2023, 7:33 PM +01:00

--- scrabble.c
+++ scrabble.c
@@ -2,21 +2,24 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int calcTotal(string word);
+int calc_total(string word);
 
-int[26] charValues = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
+int[26] char_values = [1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
 
 int main(void)
 {
+    string p1 = get_string("Player 1: ");
+    string p2 = get_string("Player 2: ");
+
 
 }
 
-int calcTotal(char[] letters)
+int calc_total(char[] letters)
 {
     int total = 0;
 
     for (char c : letters)
     {
-        int index = c.get
+        total += char_values(c - )
     }
 }
