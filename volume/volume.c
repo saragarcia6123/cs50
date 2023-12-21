da0a511f62b8bfca7fa0194623a383e61863638e
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-21 12:51:55 +0000

    Thu, Dec 21, 2023, 1:51 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,11 +35,12 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
 
-    u8bit_t header_data[HEADER_SIZE];
+    u8bit_t header_data[HEADER_SIZE] = fread(HEADER_SIZE);
+    fwrite(header_data, output);
 
     for (int i = 0; i < HEADER_SIZE; i++)
     {
-        header_data[i] = 
+        header_data[i] =
     }
 
     // TODO: Read samples from input file and write updated data to output file
