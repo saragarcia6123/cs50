a716ed5e70777889b2d5df63ce2424d7428a0e5e
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-21 13:14:28 +0000

    Thu, Dec 21, 2023, 2:14 PM +01:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,10 +43,10 @@ int main(int argc, char *argv[])
     //Read samples from input file and write updated data to output file
 
     int16_t buffer;
-    while (fread(&buffer, 0, 1, input))
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
-        fwrite(&buffer, 0, 1, output);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
     // Close files
