71b141516503f6f831edb4a7de7ded68f5228f34
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-21 13:54:25 +0000

    Thu, Dec 21, 2023, 2:54 PM +01:00

--- smiley/helpers.h
+++ smiley/helpers.h
@@ -8,7 +8,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             RGBTRIPLE current_pixel = image[j][i];
-            if (current_pixel.Blue == )
+            if (current_pixel.rgbtBlue == 0x00)
+            {
+                current_pixel.rgbtBlue = 0xff;
+                current_pixel.rgbtRed = 0xff;
+            }
         }
     }
 }
