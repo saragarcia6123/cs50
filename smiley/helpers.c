9a581d6ea6368552eabe59a9d70a7ed8fb263fce
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-21 13:57:57 +0000

    Thu, Dec 21, 2023, 2:57 PM +01:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,16 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE current_pixel = image[j][i];
+            if (current_pixel.rgbtBlue == 0x00)
+            {
+                current_pixel.rgbtBlue = 0xff;
+                current_pixel.rgbtRed = 0xff;
+            }
+        }
+    }
 }
