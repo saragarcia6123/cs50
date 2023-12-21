9a74d0b3710eb8a3ccbb1607fcd9c5b35771464d
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-21 13:56:16 +0000

    Thu, Dec 21, 2023, 2:56 PM +01:00

--- smiley/helpers.h
+++ smiley/helpers.h
@@ -1,19 +1,5 @@
 #include "bmp.h"
 
 // colorize image
-void colorize(int height, int width, RGBTRIPLE image[height][width])
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            RGBTRIPLE current_pixel = image[j][i];
-            if (current_pixel.rgbtBlue == 0x00)
-            {
-                current_pixel.rgbtBlue = 0xff;
-                current_pixel.rgbtRed = 0xff;
-            }
-        }
-    }
-}
+void colorize(int height, int width, RGBTRIPLE image[height][width]);
 
