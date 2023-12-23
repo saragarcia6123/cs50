5d0d7999a3b1229c145c08d018f8308bd3458e41
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-23 22:28:54 +0000

    Sat, Dec 23, 2023, 11:28 PM +01:00

--- pointers.c
+++ pointers.c
@@ -6,5 +6,6 @@ int main(void)
     int* p = &a;
     char* p0;
     p0 = (char*)p;
-    printf("Value of a: %i, Address of a: %d\nValue of p: %dAddress of p: %d\nValue of p0: %d, Address of p0: %d", a, &a, *p, p, *p0, p0);
+    //printf("Value of a: %i, Address of a: %d\nValue of p: %dAddress of p: %d\nValue of p0: %d, Address of p0: %d", a, &a, *p, p, *p0, p0);
+    printf("%d", &a);
 }
