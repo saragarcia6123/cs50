21246d751f4809e5ab6cb58f32478a9a36ae5790
saragarcia6123 <152648593+saragarcia6123@users.noreply.github.com>
2023-12-24 21:31:01 +0000

    Sun, Dec 24, 2023, 10:31 PM +01:00

--- pointers.c
+++ pointers.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char **argv)
 {
-    int a = 1;
-    printf("%d\n", &a);
+    int quan = 10;
+    int price = 20;
+
+    // using %d with printf()
+    printf("Price of %d notebooks is %d", quan,
+           quan * price);
+
+    return 0; 
 }
